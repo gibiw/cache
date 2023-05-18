343de2fc385804de0d14b859e0923c07e2db6307
Dmitry.Gridnev <dmitry.gridnev@testit.software>
2023-05-18 23:25:17 +0200

    Added TTL for cached record and changed mutex to RW

--- README.md
+++ README.md
@@ -20,13 +20,15 @@ func main() {
     // Create a cache
     cache := cache.New()
 
-    // Set the value of the key "userId" to 42
-    cache.Set("userId", 42)
+    // Set the value of the key "userId" to 42 and TTL 10 seconds
+    cache.Set("userId", 42, time.Second * 10)
 
     // Get the value of the key "userId"
-    if userId, ok := cache.Get("userId"); ok {
-        fmt.Println(userId)
-    }
+    userId, err := cache.Get("userId")
+    if err != nil {
+        fmt.Println(err)
+    }   
+    fmt.Println(userId)
 
     // Delete the value of the key "userId"
     cache.Delete("userId")
--- cache.go
+++ cache.go
@@ -1,41 +1,58 @@
 package cache
 
 import (
+	"errors"
 	"sync"
+	"time"
 )
 
+var (
+	ErrorNotFound error = errors.New("value not found")
+	ErrorExpired  error = errors.New("value is expired")
+)
+
+type item struct {
+	value       interface{}
+	dedlineTime time.Time
+}
+
 type cache struct {
-	storage map[string]interface{}
-	mu      sync.Mutex
+	storage map[string]item
+	mu      sync.RWMutex
 }
 
 func New() *cache {
 	return &cache{
-		storage: make(map[string]interface{}),
+		storage: make(map[string]item),
 	}
 }
 
-func (c *cache) Set(key string, value interface{}) {
+func (c *cache) Set(key string, value interface{}, ttl time.Duration) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	if _, ok := c.storage[key]; ok {
-		c.storage[key] = value
-		return
+	c.storage[key] = item{
+		value:       value,
+		dedlineTime: time.Now().Add(ttl),
 	}
-
-	c.storage[key] = value
 }
 
-func (c *cache) Get(key string) (interface{}, bool) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
+func (c *cache) Get(key string) (interface{}, error) {
+	c.mu.RLock()
 
-	if value, ok := c.storage[key]; ok {
-		return value, true
+	value, ok := c.storage[key]
+
+	c.mu.RUnlock()
+
+	if !ok {
+		return nil, ErrorNotFound
 	}
 
-	return nil, false
+	if value.dedlineTime.Before(time.Now()) {
+		return nil, ErrorExpired
+	}
+
+	return value, nil
 }
 
 func (c *cache) Delete(key string) {
